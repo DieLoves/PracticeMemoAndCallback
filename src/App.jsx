2ad80c4d440456c7a858d7339cdcbc9e00aec51e
DieLoves <insar667@gmail.com>
2025-05-17 17:59:34 +0500

    useCallback and useMemo practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,39 @@
-import { useState } from 'react'
+import { useCallback, useMemo, useState } from 'react'
 import './App.css'
 import CounterView from './components/CounterView'
 import Title from './components/Title'
 
 function App() {
 	const [count, setCount] = useState(0)
+	const [firstHook] = useState('useCallback')
+	const [secondHook] = useState('useMemo')
 
 	const incrementCount = () => {
 		setCount(count => count + 1)
 	}
 
+	// Rerender! Function resetCounter is recreating
+	// const resetCounter = () => {
+	// 	setCount(0)
+	// }
+	const resetCounter = useCallback(() => {
+		setCount(0)
+	}, [])
+
+	// const hooks = { firstHook, secondHook }
+
+	// Fix!)
+	const hooks = useMemo(
+		() => ({ firstHook, secondHook }),
+		[firstHook, secondHook]
+	)
+
 	return (
 		<>
-			<Title />
+			{/* <Title firstHook={firstHook} secondHook={secondHook} /> <-- No rerender! Value equals because they is primitive */}
+			{/* <Title hooks={} /> <-- Rerender! Objects are compared by reference */}
+			{/* <Title hooks={hooks} /> <-- Fix!) */}
+			<Title hooks={hooks} resetCounter={resetCounter} />
 			<CounterView count={count} />
 			<button onClick={incrementCount}>Click</button>
 		</>
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,11 +1,28 @@
 import { memo } from 'react'
 
-export default memo(() => {
+export default memo(({ hooks, resetCounter }) => {
 	console.log('Title render')
+	const { firstHook, secondHook } = hooks
 
 	return (
 		<>
-			<h1>React.memo | useCallback | useMemo</h1>
+			<h1>
+				React.memo | {firstHook} | {secondHook}
+			</h1>
+			{/* Bad practice? */}
+			<button onClick={resetCounter}>Reset!</button>
 		</>
 	)
 })
+
+// export default memo(({ firstHook, secondHook }) => {
+// 	console.log('Title render')
+
+// 	return (
+// 		<>
+// 			<h1>
+// 				React.memo | {firstHook} | {secondHook}
+// 			</h1>
+// 		</>
+// 	)
+// })
