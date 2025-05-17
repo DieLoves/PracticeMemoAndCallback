9e105b2ecaa79b138aef5619a6ac31cad1098948
DieLoves <insar667@gmail.com>
2025-05-17 18:38:38 +0500

    Advanced work with useMemo. Practice is over ( I guess =) )

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import { useCallback, useMemo, useState } from 'react'
-import './App.css'
 import CounterView from './components/CounterView'
 import Title from './components/Title'
+import delayedDouble from './utils/delayedDouble'
 
 function App() {
 	const [count, setCount] = useState(0)
 	const [firstHook] = useState('useCallback')
 	const [secondHook] = useState('useMemo')
+	const [dark, setDark] = useState(false)
 
+	const themeStyle = {
+		backgroundColor: dark ? '#000' : '#242424',
+	}
 	const incrementCount = () => {
 		setCount(count => count + 1)
+	}
+
+	const switchTheme = () => {
+		setDark(dark => !dark)
 	}
 
 	// Rerender! Function resetCounter is recreating
@@ -28,15 +36,21 @@ function App() {
 		[firstHook, secondHook]
 	)
 
+	const doubleCount = useMemo(() => delayedDouble(count), [count])
+
 	return (
-		<>
+		<div className='app' style={{ ...themeStyle }}>
 			{/* <Title firstHook={firstHook} secondHook={secondHook} /> <-- No rerender! Value equals because they is primitive */}
 			{/* <Title hooks={} /> <-- Rerender! Objects are compared by reference */}
 			{/* <Title hooks={hooks} /> <-- Fix!) */}
 			<Title hooks={hooks} resetCounter={resetCounter} />
-			<CounterView count={count} />
+			<CounterView count={doubleCount} />
 			<button onClick={incrementCount}>Click</button>
-		</>
+			<br />
+			<button style={{ margin: '20px 0 0' }} onClick={switchTheme}>
+				Switch theme
+			</button>
+		</div>
 	)
 }
 
---
+++ src/utils/delayedDouble.js
@@ -0,0 +1,4 @@
+export default function (num) {
+	for (let i = 0; i < 1999999999; i++);
+	return (num *= 2)
+}
